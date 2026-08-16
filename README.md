66c0981d86d3feeed801477db8c4c3c152261510
Ange Emmanuelle <yourbinelintsa@gmail.com>
2026-08-16 04:59:54 -0700

    Setup_project.sh script and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# deploy_agent_binelintsa-sketch
-Automated project Bootstraping and process management.
+Deploy_agent_binelintsa-sketch
+Project Setup and Process Automation
+
+Script Automation. 
+
+#Overview
+ setup_project.sh automates the initialization of attendance tracking systems. It creates parent directory (attendance_tracker_(name) and sub-directory tructures(helpers and reports). the script will prompt the user if he wants to update the threshold values of attendance tracker found in config.json by asking entering yes  (y) or No (Y). if the answer is yes the user will prompt the warning and failure values esle the script will skip this step and used the default values using the sed -i command. this will configures the environment thresholds, and finally  verifies if python3 is installed on the system. The code os built in with a SIGINT in case you started your setup with a wrong name or you just want to delete everything.
+
+#Expected result; After running the script, the output should look like this.
+
+attendance_tracker_<name>/
+├── attendance_checker.py
+├── Helpers/
+│   ├── assets.csv
+│   └── config.json
+└── reports/
+    └── reports.log
+
+#How does the SIGINT function
+In case the user changed his mind or a trigger happens the user user can prees Ctrl+c and the created directory will be automatically deleted and save as an archive (attendance_tracker_(name)_archive.Zip. Thus it deletes directory that is incomplete.
---
+++ setup_project.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# 1-define the cleanup function
+cleanup() {
+        echo -e "\nprocess interrupted! saving and cleaning up..."
+        # check if directory exist before saving
+        if [ -n "$DIR_NAME" ] && [ -d "attendance_tracker_${DIR_NAME}" ]; then
+        	tar -czf "attendance_tracker_${DIR_NAME}_archive.tar.gz" "attendance_tracker_${DIR_NAME}"
+		rm -rf "attendance_tracker_${DIR_NAME}"
+       	fi
+	exit 1
+}
+
+# 2-setting the trap
+trap cleanup SIGINT
+
+# 3-print input
+read -p "Enter project identifier: " DIR_NAME
+
+#directory is being created, files are copied
+mkdir -p "attendance_tracker_${DIR_NAME}/Helpers"
+mkdir -p "attendance_tracker_${DIR_NAME}/reports"
+
+#files are being copied to their respective directotry
+cp attendance_checker.py "attendance_tracker_${DIR_NAME}/"
+cp assets.csv config.json "attendance_tracker_${DIR_NAME}/Helpers/"
+cp reports.log "attendance_tracker_${DIR_NAME}/reports/"
+
+#ask the user if he wants to update the threshold values
+read -p "Do you want to update attendance thresholds? (y/n): "Choice"
+
+#check if the answer is "y" or "Y"
+if [[ "$Choice" =~ ^[Yy]$ ]]; then
+
+	#the user will prompt the new valeus using read if based on his choice
+	read -p "Enter Warning threshold (default 75%): " WARN_VAL
+	read -p "Enter Failure threshold (default 50%): " FAIL_VAL
+
+	#if n values where entered the code will find default numbers in config.json using sed -i
+	sed -i "s/75/$WARN_VAL}/g" "attendance_tracker_${DIR_NAME}/Helpers/config.json"
+	sed -i "s/50/$FAIL_VAL}/g" "attendance_tracker_${DIR_NAME}/Helpers/config.json"
+	echo "Thresholds values updated succesfully."
+else
+	echo "default values retained."
+fi 
+#checking if python3 exist
+	if python3 --version &> /dev/null; then
+		echo "Health Check Passed: $(python3 --version) is  installed."
+	else 
+		echo "Warning: python3 is not installed on this computer."
+	fi
